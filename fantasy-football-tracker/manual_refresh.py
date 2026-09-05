c477c03af5c0183ec27f9bf30ca66293e99bfc1d
Will Blatz <willblatz97@gmail.com>
2026-09-05 10:05:13 -0400

    Trigger Saturday fantasy tracker refresh

--- fantasy-football-tracker/manual_refresh.py
+++ fantasy-football-tracker/manual_refresh.py
@@ -1,3 +1,3 @@
 # Manual Fantasy War Room refresh trigger.
 # Updating this file intentionally triggers the GitHub Actions refresh workflow.
-# Last requested refresh: 2026-08-31T17:40:00Z
+# Last requested refresh: 2026-09-05T14:05:09Z
